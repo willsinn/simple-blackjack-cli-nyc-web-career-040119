480511080c0b31d5ff8d6d8f2b3b1d4a123de52d
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 04:14:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,7 +57,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
-end
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)end
     
 
